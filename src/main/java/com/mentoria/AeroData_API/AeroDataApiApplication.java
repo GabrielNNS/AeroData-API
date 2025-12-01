da8235c9a6324c39debbddfd14a5922e1bfe1564
GabrielNNS <gabriel.nascimento33@hotmail.com>
2025-12-01 12:36:52 -0300

    initial commit

---
+++ src/main/java/com/mentoria/AeroData_API/AeroDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.mentoria.AeroData_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentoria/AeroData_API/AeroDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoria.AeroData_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
